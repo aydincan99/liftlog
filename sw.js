70b308059614594d3d69d28bfffb80595737b756
aydincan99 <aydincan99@users.noreply.github.com>
2026-08-30 19:27:01 +0300

    Liftlog PWA

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'liftlog-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((hit) => hit || caches.match('./'))),
+  );
+});
